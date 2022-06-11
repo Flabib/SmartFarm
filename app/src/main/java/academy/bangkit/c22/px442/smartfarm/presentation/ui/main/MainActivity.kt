f2536c73a76a16bda6e26d3c8227403e77c36abb
Flabib <fahdilabib@gmail.com>
2022-06-11 18:49:48 +0700

    Add Login Activity and Re-Enable IntroActivity

--- app/src/main/java/academy/bangkit/c22/px442/smartfarm/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/academy/bangkit/c22/px442/smartfarm/presentation/ui/main/MainActivity.kt
@@ -2,21 +2,40 @@ package academy.bangkit.c22.px442.smartfarm.presentation.ui.main
 
 import academy.bangkit.c22.px442.smartfarm.R
 import academy.bangkit.c22.px442.smartfarm.databinding.ActivityMainBinding
+import academy.bangkit.c22.px442.smartfarm.presentation.ui.login.LoginActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private var menu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
+
+        Snackbar.make(binding.root, "Welcome, ${firebaseUser.displayName}!", Snackbar.LENGTH_SHORT).show()
 
         setSupportActionBar(binding.toolbar)
 
@@ -26,6 +45,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        this.menu = menu;
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
